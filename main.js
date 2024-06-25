ba89dc49860fd0bbe96b1ec274ee2b4aa2240490
nsreenjo <nsreenjoundya70@gmail.com>
2024-06-25 20:46:26 +0300

    task1js

---
+++ main.js
@@ -0,0 +1,118 @@
+
+// task2
+
+console.log(typeof(100));
+
+console.log(typeof(73.9));
+
+console.log(typeof(NaN));
+
+console.log(typeof("Water"));
+
+console.log(typeof(9 != 11));
+
+console.log("Orang"+"e");
+
+console.log("Orange"-"s");
+
+console.log("4"+"8");
+
+console.log("4"-"8");
+
+console.log("name"+ 3);
+
+console.log("name"-3);
+
+console.log(82 * "word");
+
+console.log(1 +"hello");
+
+console.log("hello"+ 1);
+
+console.log(1 +true);
+
+console.log("hello" + true);
+
+console.log(typeof (Infinity) );
+
+console.log(1=='1');
+
+console.log(1==='1');
+
+// task3
+
+
+let text = "Welcome to Orange"
+
+console.log(text.toUpperCase())
+
+console.log(text.slice(8,10).toUpperCase())
+
+console.log(text.replace("Welcome", "Hello from"));
+
+console.log(text.toLowerCase());
+
+console.log(text.length);
+
+console.log(text+" Jordan")
+
+
+ //task4//
+ window.alert("welcome");
+
+ //task5//
+ let string1 = "Hello";
+ console.log(string1);
+let number = 77;
+console.log(number1);
+let boolean1 = true;
+console.log(boolean1);
+let array1 = [1, 2, 3,"zaina"];
+console.log(array1);
+let object1 = { name: "zaina", age: 20 };
+console.log(object1);
+
+//tassk6//
+console.log("ayah")
+if (true) {
+    var varValue = "This is a var variable";
+}
+ //task7//
+
+ var x = 10;
+ var b = 5;
+
+ console.log(x + b);
+
+ console.log(x - b);
+
+ console.log(x * b);
+
+ console.log(x / b);
+
+ console.log(x % b);
+
+ console.log(x > b);
+
+ console.log(x < b);
+
+ console.log(x == b );
+
+ console.log(x != b );
+
+ console.log(x >= b );
+
+ console.log(x <= b );
+
+ console.log("AND:", x && b );
+
+ console.log("OR:", x || b );
+
+ console.log("NOT:", !x && !b );
+
+ console.log(x += 5 );
+
+ console.log(x -= 5 );
+ 
+ console.log(x *= 2);
+ 
